a7304b546997652d8f6a4979d7fcf596f2f87323
Neomonolith <90251446+Neomonolith@users.noreply.github.com>
2022-01-24 11:59:18 +0100

    finale

--- DHCPfeladat/Program.cs
+++ DHCPfeladat/Program.cs
@@ -18,20 +18,67 @@ namespace DHCPfeladat
 
     static void Main(string[] args)
     {
-      exclud =new List<int>();
+      exclud = new List<int>();
       reserved = new Dictionary<string, int>();
       DHCP = new Dictionary<string, int>();
 
       beolvas();
       testfajl();
       kiir();
-
+      KonzolosKiir();
       Console.ReadKey();
+    }
+
+    private static void KonzolosKiir()
+    {
+      foreach (var D in DHCP)
+      {
+        Console.WriteLine("{0};{1} {2}", D.Key, induloIPcim, D.Value);
+      }
     }
 
     private static void testfajl()
     {
+      foreach (string testI in File.ReadAllLines("test.csv"))
+      {
+        string tipus = testI.Split(';')[0];
+        if (tipus == "request")
+        {
+          string mac = testI.Split(';')[1];
+          if (!DHCP.ContainsKey(mac))
+          {
+            if (reserved.ContainsKey(mac))
+            {
+              int ip = reserved[mac];
+              if (!DHCP.ContainsValue(ip))
+              {
+                DHCP.Add(mac, ip);
+              }
+            }
+            else
+            {
 
+              while (elsoIP > utsoIP)
+              {
+                if (!DHCP.ContainsValue(elsoIP) && !exclud.Contains(elsoIP) && !reserved.ContainsValue(elsoIP))
+                {
+                  DHCP.Add(mac, elsoIP);
+                  break;
+                }
+                elsoIP++;
+              }
+            }
+          }
+        }
+        else if (tipus == "release")
+        {
+          int ip = int.Parse(testI.Split(';', '.')[4]);
+          if (DHCP.ContainsValue(ip))
+          {
+            DHCP.Remove(DHCP.First(d => d.Value == ip).Key);
+          }
+        }
+      }
 
     }
 
@@ -74,7 +121,7 @@ namespace DHCPfeladat
       // // reserved.Add((b[0],b[1],b[2],b[3], int.Parse(b[4])));                                                      //\\//\\            //\\//\\
       // // reserved.Add(b.Split(';')[0], int.Parse(a.Split(';', '.')[4]));                                            //\\//\\    KULA    //\\//\\
       //}                                                                                                              //\\//\\            //\\//\\
-      //be2.Close();                                                                                                   //\\//\\  ( ﾉ ﾟｰﾟ)ﾉ //\\//\\
+      //be2.Close();                                                                                                   //\\//\\            //\\//\\
       //                                                                                                               //\\//\\            //\\//\\
       //foreach (string item in File.WriteAllLines("reserved.csv"))                                                    //\\//\\            //\\//\\
       //{                                                                                                              //\\//\\            //\\//\\
