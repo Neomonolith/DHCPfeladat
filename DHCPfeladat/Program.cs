98e23573f557d54c16e61284921ea54652fff5b1
Neomonolith <90251446+Neomonolith@users.noreply.github.com>
2022-01-24 09:12:50 +0100

    C# tartományok létrehozása

--- DHCPfeladat/Program.cs
+++ DHCPfeladat/Program.cs
@@ -3,13 +3,25 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.IO;
 
 namespace DHCPfeladat
 {
   class Program
   {
+    static Dictionary<string, int> dhcp;
+    static Dictionary<string, int> reserved;
+    static List<int> exclud;
+    static string induloIPcim = "192.168.10.";
+    static int elsoIP = 100;
+    static int utsoIP = 199;
+
     static void Main(string[] args)
     {
+      exclud =new List<int>();
+      reserved = new Dictionary<string, int>();
+      dhcp = new Dictionary<string, int>();
+
     }
   }
 }
